de16843cfc373f200cb169ba5e700ac50620f33e
millatur <minnatur7373@gmail.com>
2024-03-19 05:30:22 +0600

    yegdy

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,20 +14,32 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
           <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Full name">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Full name"
+            required value="{{ old('name') }}">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-user"></span>
               </div>
             </div>
+            @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
           </div>
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email">
+            <input type="email" name="email" class="form-control @error('name') is-invalid @enderror" placeholder="Email"
+            required value="{{ old('email') }}">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
               </div>
             </div>
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
           </div>
           <div class="input-group mb-3">
             <input type="password" class="form-control" placeholder="Password">
