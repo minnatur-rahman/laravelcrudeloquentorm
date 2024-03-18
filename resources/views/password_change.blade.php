8ca2d6d4b9fb8929a1d84daf112bffba7a3ea8f6
millatur <minnatur7373@gmail.com>
2024-03-18 22:49:13 +0600

    dhgysdg

--- resources/views/password_change.blade.php
+++ resources/views/password_change.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-<div class="container">
+<div class="container my-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
