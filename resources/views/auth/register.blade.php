f1d45c67b88e6a4e13ddc921e04e7cad3d2012b0
millatur <minnatur7373@gmail.com>
2024-03-19 05:57:52 +0600

    ytewygh

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,15 +42,21 @@
             @enderror
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"
+            required autocomplete="new-password" placeholder="Password">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
               </div>
             </div>
+            @error('password')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Retype password">
+            <input id="password-confirm" type="password" class="form-control" placeholder="Retype Password" name="password_confirmation" required autocomplete="new-password">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
