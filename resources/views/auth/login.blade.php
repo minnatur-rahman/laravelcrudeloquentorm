efd02659b03b517b538b93795831ba62709008f1
millatur <minnatur7373@gmail.com>
2024-03-19 19:06:04 +0600

    gdysgtd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,7 +69,7 @@
         <!-- /.social-auth-links -->
 
         <p class="mb-1">
-          <a href="#">I forgot my password</a>
+          <a href="{{ route('password.request') }}">I forgot my password</a>
         </p>
         <p class="mb-0">
           <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
