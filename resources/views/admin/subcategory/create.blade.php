bd8331adc88b2e41ca35d6e68675b7e6fa846daa
millatur <minnatur7373@gmail.com>
2024-03-23 05:23:21 +0600

    ueydw

--- resources/views/admin/subcategory/create.blade.php
+++ resources/views/admin/subcategory/create.blade.php
@@ -7,20 +7,21 @@
             <div class="card">
                 <div class="card-header">{{ __('Add New SubCategory') }}</div>
                   <div class="card-body">
-                    <form action="{{ route('subcategory.store') }}" method="POST">
+                    <form action="{{ route('category.store') }}" method="POST">
                         @csrf
                          <div class="form-group">
                             <label for="disabledTextInput" class="form-label">Choose Category</label>
-                              <select>
-                                @fo
-                                <option></option>
+                              <select class="form-controll" name="category_id">
+                                 @foreach ($categories as $row )
+                                   <option value="{{ $row->id }}"> {{ $row->category_name }}</option>
+                                 @endforeach
                               </select>
                           </div>
                           <div class="form-group">
                             <label for="disabledTextInput" class="form-label">SubCategory Name</label>
                             <input type="text" name="subcategory_name" id="disabledTextInput"
                             class="form-control @error('subcategory_name') is-invalid @enderror" value="{{ old('subcatetory_name') }}"
-                            placeholder="Category Name">
+                            placeholder="SubCategory Name">
                             @error('subcategory_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
