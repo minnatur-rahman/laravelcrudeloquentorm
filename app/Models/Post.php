eab6d6e62163b10bc24ea049a682ef802947f7b2
millatur <minnatur7373@gmail.com>
2024-03-27 05:21:36 +0600

    duhfyud

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = [
-        'category_id','subcategory_name', 'subcategory_slug'
+        'category_id','subcategory_id', 'title', 'slug', 'post_date', 'description', 'image',
     ];
 
 
