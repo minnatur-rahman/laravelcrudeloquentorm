278f5e8760dcfe03b699af9941d367c6153e7247
millatur <minnatur7373@gmail.com>
2024-03-27 05:32:59 +0600

    ishdwshg

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\support\Str;
+use DB;
 
 class PostController extends Controller
 {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\Subcategory;
+use App\Models\User;
 
 class Post extends Model
 {
@@ -13,6 +16,16 @@ class Post extends Model
         'category_id','subcategory_id', 'title', 'slug', 'post_date', 'description', 'image',
     ];
 
+    //___join with category___//
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id'); //category_id
+    }
 
+    //___join with subcategory___//
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class, 'subcategory_id'); //category_id
+    }
 
 }
