2dbe50d1f6bcbfe4e75da13b7e287e0c992a2937
millatur <minnatur7373@gmail.com>
2024-03-27 19:01:27 +0600

    dgfdh

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -5,9 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Add New SubCategory') }}</div>
+                <div class="card-header">{{ __('Add New Post') }}</div>
                   <div class="card-body">
-                    <form action="{{ route('subcategory.store') }}" method="POST">
+                    <form action="#" method="POST" enctype="multipart/form-data">
                         @csrf
                          <div class="form-group">
                             <label for="disabledTextInput" class="form-label">Choose Category</label>
@@ -15,14 +15,15 @@
                                  {{-- @foreach ($categories as $row )
                                    <option value="{{ $row->id }}"> {{ $row->category_name }}</option>
                                  @endforeach --}}
+                                 <option value="">Choose Category</option>
                               </select>
                           </div>
                           <div class="form-group">
-                            <label for="disabledTextInput" class="form-label">SubCategory Name</label>
-                            <input type="text" name="subcategory_name" id="disabledTextInput"
-                            class="form-control @error('subcategory_name') is-invalid @enderror" value="{{ old('subcatetory_name') }}"
-                            placeholder="SubCategory Name">
-                            @error('subcategory_name')
+                            <label for="disabledTextInput" class="form-label">Title</label>
+                            <input type="text" name="title" id="disabledTextInput"
+                            class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}"
+                            placeholder="Post Title">
+                            @error('title')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
