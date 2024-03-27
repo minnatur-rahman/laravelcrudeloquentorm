ecab640683a4335d75fe8e9bfcea2dbf8a929318
millatur <minnatur7373@gmail.com>
2024-03-27 19:12:23 +0600

    iuhydush

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -4,34 +4,45 @@
 <div class="container mt-5 ">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Add New Post') }}</div>
-                  <div class="card-body">
-                    <form action="#" method="POST" enctype="multipart/form-data">
-                        @csrf
-                         <div class="form-group">
-                            <label for="disabledTextInput" class="form-label">Choose Category</label>
-                              <select class="form-controll" name="category_id">
-                                 {{-- @foreach ($categories as $row )
-                                   <option value="{{ $row->id }}"> {{ $row->category_name }}</option>
-                                 @endforeach --}}
-                                 <option value="">Choose Category</option>
-                              </select>
-                          </div>
-                          <div class="form-group">
-                            <label for="disabledTextInput" class="form-label">Title</label>
-                            <input type="text" name="title" id="disabledTextInput"
-                            class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}"
-                            placeholder="Post Title">
-                            @error('title')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                          </div>
-                          <button type="submit" class="btn btn-primary">Submit</button>
-                      </form>
+            <div class="card card-primary">
+                <div class="card-header">
+                  <h3 class="card-title">Quick Example</h3>
                 </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form>
+                  <div class="card-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputFile">File input</label>
+                      <div class="input-group">
+                        <div class="custom-file">
+                          <input type="file" class="custom-file-input" id="exampleInputFile">
+                          <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                        </div>
+                        <div class="input-group-append">
+                          <span class="input-group-text">Upload</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                  </div>
+                  <!-- /.card-body -->
+
+                  <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </form>
               </div>
             </div>
         </div>
