b418568bad4b5ca8d8286ebce7f4eebfbaa6cd40
millatur <minnatur7373@gmail.com>
2024-03-30 19:03:30 +0600

    uasdhuyyh

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -15,8 +15,8 @@
                   <div class="card-body">
                     <div class="form-group">
                       <label for="exampleInputEmail4">Post Title</label>
-                      <input type="text" class="form-control" id="exampleInputEmail4" name="title"
-                      value="{{ $post->title }}" required>
+                      <input type="text" class="form-control" value="{{ $post->title }}" name="title"
+                      placeholder="Post Title" required>
                     </div>
                     <div class="form-group">
                       <label for="exampleInputPassword3">Category</label>
@@ -28,7 +28,7 @@
                         @endphp
                             <option disabled class="text-danger">{{$cat->category_name}}</option>
                             @foreach ($subcategories as $sub )
-                               <option value="{{ $sub->id }}" class="text-success">----{{$sub->subcategory_name}}</option>
+                               <option value="{{ $sub->id }}" class="text-success" @if($sub->id==$post->subcategory_id) selected @endif>----{{$sub->subcategory_name}}</option>
                             @endforeach
                         @endforeach
 
@@ -42,15 +42,15 @@
                       </div> --}}
                       <div class="form-group">
                         <label for="exampleInpu1tPassword1">Post Date</label>
-                        <input type="date" name="post_date" id="exampleInpu1tPassword1" required class="form-control">
+                        <input type="date" name="post_date" id="exampleInpu1tPassword1" required class="form-control" value="{{ $post->post_date }}">
                       </div>
                       <div class="form-group">
                         <label for="exampleInputPa3ssword1">Tages</label>
-                        <input type="text" name="tags" class="form-control" id="exampleInputPa3ssword1" required>
+                        <input type="text" name="tags" class="form-control" id="exampleInputPa3ssword1" required value="{{ $post->tags }}">
                       </div>
                       <div class="form-group">
                         <label for="summernote">Description</label>
-                        <textarea class="form-control summernote" name="description"  cols="30" rows="10"></textarea>
+                        <textarea class="form-control summernote" name="description" rows="4">{{ $post->description }}</textarea>
                       </div>
                     <div class="form-group">
                       <label for="exampleInputFile">File Input</label>
@@ -59,20 +59,21 @@
                           <input type="file" name="image" class="custom-file-input" id="exampleInputFil4e">
                           <label class="custom-file-label" for="exampleInputFil4e">Choose file</label>
                         </div>
+                        <input type="hidden" name="old_image" value="{{ $post->image }}">
                         <div class="input-group-append">
                           <span class="input-group-text">Upload</span>
                         </div>
                       </div>
                     </div>
                     <div class="form-check">
-                      <input type="checkbox" name="status" value="1" class="form-check-input" id="exampleCheck1">
+                      <input type="checkbox" name="status" value="1" class="form-check-input" @if($post->status==1) checked @endif id="exampleCheck1">
                       <label class="form-check-label" for="exampleCheck1">Publish Now </label>
                     </div>
                   </div>
                   <!-- /.card-body -->
 
                   <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
                   </div>
                 </form>
               </div>
